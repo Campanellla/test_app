7f80120e1b793309edc836a9a5b5645daf40a57b
Campanellla <afkis.afkis@gmail.com>
2020-08-25 15:48:44 +0300

    feat: date filter

--- src/api/resolvers/queries/listItems.ts
+++ src/api/resolvers/queries/listItems.ts
@@ -11,18 +11,18 @@ const listItems = async (_, args) => {
     sorting = JSON.parse(sort)
   } catch (e) {}
 
-  const { onlyAppartments, onlyVouchers, param = 'name' } = sorting
+  const { type, pricestart, priceend, datestart, dateend, rooms, param = 'name' } = sorting
 
-  const _vouchers = onlyAppartments ? [] : await Voucher.find({})
-  const _appartments = onlyVouchers ? [] : await Appartment.find({})
+  const _vouchers = type === 'appartments' ? [] : await Voucher.find({})
+  const _appartments = type === 'vouchers' ? [] : await Appartment.find({})
 
   const promisesV = _vouchers.map((voucher) => serializeVoucher(voucher))
   const promisesA = _appartments.map((app) => serializeAppartment(app))
 
-  const vouchers = onlyAppartments ? ([] as unknown[]) : await Promise.all(promisesV)
-  const appartments = onlyVouchers ? ([] as unknown[]) : await Promise.all(promisesA)
+  const vouchers = type === 'appartments' ? ([] as unknown[]) : await Promise.all(promisesV)
+  const appartments = type === 'vouchers' ? ([] as unknown[]) : await Promise.all(promisesA)
 
-  const items: ({ appartment: any; voucher: null } | { voucher: any; appartment: null })[] = [
+  let items: ({ appartment: any; voucher: null } | { voucher: any; appartment: null })[] = [
     ...vouchers.map((voucher: any) => ({ voucher, appartment: null })),
     ...appartments.map((appartment: any) => ({ appartment, voucher: null })),
   ]
@@ -36,6 +36,33 @@ const listItems = async (_, args) => {
     items.sort((a, b) =>
       (a.appartment || a.voucher)?.name >= (b.appartment || b.voucher)?.[param] ? 0 : 1
     )
+
+  if (Number(pricestart) > 0)
+    items = items.filter((item) => (item.appartment || item.voucher)?.price > Number(pricestart))
+
+  if (Number(priceend) > 0)
+    items = items.filter((item) => (item.appartment || item.voucher)?.price < Number(priceend))
+
+  if (Number(rooms) > 0) items = items.filter((item) => item.appartment?.rooms === Number(rooms))
+
+  if (datestart || dateend)
+    items = items.filter((item) => {
+      const slots = item.appartment?.timeSlots?.filter((slot) => {
+        if (!slot?.start) return false
+
+        const start = new Date(slot.start)
+        const end = new Date(slot.end)
+
+        if (datestart && start < new Date(datestart)) return false
+
+        if (dateend && end > new Date(dateend)) return false
+
+        return true
+      })
+      if (slots?.length > 0) return true
+
+      return false
+    })
 
   return items
 }
--- src/components/ListItems.tsx
+++ src/components/ListItems.tsx
@@ -5,26 +5,130 @@ import listItems from 'src/graphql/query/listItems.graphql'
 import AppartmentCard from 'src/components/Cards/AppartmentCard'
 import VoucherCard from 'src/components/Cards/VoucherCard'
 
+import { Dropdown, Button, Input, Checkbox } from 'semantic-ui-react'
+
+import { useRouter } from 'next/router'
+
+import DatePicker from 'react-datepicker'
+
 export default function Main() {
+  const router = useRouter()
+
+  const { pathname, query } = router
+
+  const sortObject = {}
+  if (query.type === 'appartments' || query.type === 'vouchers') sortObject.type = query.type
+
+  if (Number(query.pricestart) > 0) sortObject.pricestart = query.pricestart
+  if (Number(query.priceend) > 0) sortObject.priceend = query.priceend
+
+  if (query.datestart) sortObject.datestart = query.datestart
+  if (query.dateend) sortObject.dateend = query.dateend
+
+  if (Number(query.rooms) > 0) sortObject.rooms = query.rooms
+
   const { loading, data, error } = useQuery(listItems, {
-    variables: { sort: `{"onlyAppartments": false}` },
+    variables: { sort: JSON.stringify(sortObject) },
   })
+
+  const [type, setType] = React.useState(sortObject.type || 'all')
+
+  const [priceStart, setPriceStart] = React.useState(null)
+  const [priceEnd, setPriceEnd] = React.useState(null)
+
+  const [dateStart, setDateStart] = React.useState(new Date())
+  const [dateEnd, setDateEnd] = React.useState(new Date())
+  const [rooms, setRooms] = React.useState(null)
+
+  const [timeFilter, setTimeFilter] = React.useState(false)
 
   if (loading) return <div>loading</div>
 
   const items = data?.listItems
 
-  console.log(items)
-
   if (error) return <div>Get appartments error</div>
+
+  const filter = () => {
+    const query = {}
+
+    if (type) query.type = type
+    if (priceStart > 0) query.pricestart = priceStart
+    if (priceEnd > 0) query.priceend = priceEnd
+
+    if (dateStart && timeFilter) query.datestart = dateStart.toISOString()
+    if (dateEnd && timeFilter) query.dateend = dateEnd.toISOString()
+    if (rooms) query.rooms = rooms
+
+    const href = `${pathname}?${new URLSearchParams(query).toString()}`
+
+    router.push(href)
+  }
 
   return (
     <div>
-      <select>
-        <option>all</option>
-        <option>appartments</option>
-        <option>vouchers</option>
-      </select>
+      <Dropdown
+        selection
+        value={type}
+        options={[
+          {
+            key: 'all',
+            text: 'all',
+            value: 'all',
+          },
+          {
+            key: 'appartments',
+            text: 'appartments',
+            value: 'appartments',
+          },
+          {
+            key: 'vouchers',
+            text: 'vouchers',
+            value: 'vouchers',
+          },
+        ]}
+        onChange={(_, e) => setType(e.value as string)}
+      />
+
+      <Input
+        type="number"
+        value={priceStart}
+        onChange={(e) => setPriceStart(e.currentTarget.value)}
+      />
+
+      <Input type="number" value={priceEnd} onChange={(e) => setPriceEnd(e.currentTarget.value)} />
+
+      <Input type="number" value={rooms} onChange={(e) => setRooms(e.currentTarget.value)} />
+
+      <div>
+        <Checkbox
+          checked={timeFilter}
+          label="filter by time range"
+          onChange={(_, e) => setTimeFilter(e.checked)}
+        />
+        Start time:
+        <DatePicker
+          selected={dateStart}
+          onChange={(date) => setDateStart(date)}
+          timeInputLabel="Time start:"
+          dateFormat="MM/dd/yyyy h:mm aa"
+          showTimeInput
+          timeIntervals={60}
+          showTimeSelect
+        />
+        End time:
+        <DatePicker
+          selected={dateEnd}
+          onChange={(date) => setDateEnd(date)}
+          timeInputLabel="Time end:"
+          dateFormat="MM/dd/yyyy h:mm aa"
+          showTimeInput
+          timeIntervals={60}
+          showTimeSelect
+          minDate={dateStart}
+        />
+      </div>
+
+      <Button onClick={filter}> Filter </Button>
 
       {items.map((item) => {
         if (item?.appartment)
