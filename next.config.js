930c165b4c8418ff98626c1d95c89fba2130a2bb
Campanellla <afkis.afkis@gmail.com>
2020-08-25 16:38:04 +0300

    fix: dirname

--- next.config.js
+++ next.config.js
@@ -2,15 +2,18 @@
 const graphqlLoader = {
   test: /\.(graphql|gql)$/,
   exclude: /node_modules/,
-  loader: "graphql-tag/loader",
-};
+  loader: 'graphql-tag/loader',
+}
 
 module.exports = {
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   webpack: (config) => {
-    config.module.rules.push(graphqlLoader);
-    return config;
+    config.module.rules.push(graphqlLoader)
+    return config
   },
-};
+}
--- pages/api/apollo.ts
+++ pages/api/apollo.ts
@@ -1,24 +1,27 @@
-import { ApolloServer } from "apollo-server-micro";
-import { importSchema } from "graphql-import";
-import path from "path";
-import resolvers from "src/api/resolvers";
-import mongoStart from "src/api/connectors/mongo";
-import context from "src/api/context/context";
+import { ApolloServer } from 'apollo-server-micro'
+import { importSchema } from 'graphql-import'
+import path from 'path'
+import resolvers from 'src/api/resolvers'
+import mongoStart from 'src/api/connectors/mongo'
+import context from 'src/api/context/context'
 
-mongoStart();
+import getConfig from 'next/config'
+const { serverRuntimeConfig } = getConfig()
+
+mongoStart()
 
 const typeDefs = importSchema(
-  path.join(process.env.PWD, "src/api/schema/schema.graphql")
-);
+  path.join(serverRuntimeConfig.PROJECT_ROOT, 'src/api/schema/schema.graphql')
+)
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers, context });
+const apolloServer = new ApolloServer({ typeDefs, resolvers, context })
 
-const handler = apolloServer.createHandler({ path: "/api/apollo" });
+const handler = apolloServer.createHandler({ path: '/api/apollo' })
 
 export const config = {
   api: {
     bodyParser: false,
   },
-};
+}
 
-export default handler;
+export default handler
